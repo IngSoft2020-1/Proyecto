1308da89d41e98179e67ae75ccac67edb62649ce
jrg131298 <42877387+jrg131298@users.noreply.github.com>
2020-04-15 18:40:08 -0700

    BackEnd
    N/A

---
+++ Web/inicioS.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="es">
+	<head>
+		<meta charset="utf-8" />
+		<title>ITT</title>
+		 <!-- Estilos CSS -->
+		 <link rel="stylesheet" type="text/css" href="StyleI.css" />
+	</head>
+	<body>
+        <script>
+            alert("Eso super mamona");
+        </script>
+	</body>
+</html>
---
+++ Web/validar.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+
+	/*Conexion de DB*/
+	$conexion=mysqli_connect("localhost","root","","itt") or
+    die("Problemas con la conexión");
+
+	/*Busca que exista el usuario*/
+	$registros=mysqli_query($conexion,"select *
+	                        from profesores where control='$_REQUEST[usuario]' and clave='$_REQUEST[clave]'") or
+	  die("Problemas en el select:".mysqli_error($conexion));
+	
+	$resultado = mysqli_num_rows($registros); /*Se almacena la cantidad de rows que se encontraron con esos datos en la DB*/
+	$res = mysqli_fetch_array($registros); /*Se almacena los datos encontrados de la DB*/
+	
+	if($resultado > 0) /*Si se encontro el usuario y la contrasena redirecciona*/
+	{
+		$_SESSION['usuario'] = $_REQUEST[usuario];
+		if($res['admin']=="si" || $res['admin']=="Si" || $res['admin']=="SI") /*En mi tabla tengo un campo como admin, el cual se guarda un si o un no, dependiendo si eres admin*/
+		{
+			header("location:inicioS.php"); /*Te redirecciona a la pagina de admin*/
+		}
+		else{
+			header("location:inicioU.php"); /*Te redirecciona a la pagina de usuario*/
+		}
+	}
+	else{ /*Si no se encontro el usuario y la contrasena redirecciona*/
+		$_SESSION['usuario'] = '';
+		header("location:login.php"); /*Regresa al login*/
+	}
+	/*Se libera el espacio y cierre de conexion*/
+	mysqli_free_result($registros);
+	mysqli_close($conexion);
+?>
