0554f44828a5e20cfd6cdba1a3cfff29375187ec
AbnerPerales19 <42877917+AbnerPerales19@users.noreply.github.com>
2020-05-01 23:28:23 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/IngSoft2020-1/Proyecto""
    This reverts commit 2ce0377bc8169b5948adb669911d3ce889205374.

--- Web-2/js/app.js
+++ Web-2/js/app.js
@@ -6,6 +6,22 @@ $(document).ready(function() {
     console.log('jQuery esta funcionando');
     obtener();
 
+    // METODO PARA MOSTRAR DATOS DE MANERA
+      $("#search-res").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#table-reservation tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+
+      // ESTE ES PARA EL USUARIO
+      $("#search-users").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#table-users tr").filter(function() {
+          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+
     /*No implementado*/
     /*En caso de hacer una busqueda de usuario por textbox*/
     $('#buscar').keyup(function(e) {
